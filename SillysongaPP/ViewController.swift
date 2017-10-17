d02860fec31976005d16b52b51935bde377b3e34
Vidya Durvasula <vidya.sl.durvasula@gmail.com>
2017-10-17 14:47:46 -0400

    addingsillysong

--- SillysongaPP/ViewController.swift
+++ SillysongaPP/ViewController.swift
@@ -7,19 +7,81 @@
 //
 
 import UIKit
+func displayshortname(name:String) -> String {
+    
+   let shortname = name.lowercased()
+    let vowelset = CharacterSet(charactersIn: "aeiou")
+    if let othername = shortname.rangeOfCharacter(from: vowelset)?.lowerBound {
+        return shortname.substring(from:othername)
+      
+    }else{
+     return shortname
+    }
+    
+}
+    func lyricforname(fullname:String,template:String) -> String{
+        
+                let shortname = displayshortname(name: fullname)
+        let lyrics = template
+            .replacingOccurrences(of: "<SHORT_NAME>", with: shortname)
+        .replacingOccurrences(of: "<FULL_NAME>", with: fullname)
+        return lyrics
+    }
+class ViewController: UIViewController,UITextFieldDelegate{
+    
+        @IBOutlet var lyricsView: UITextView!
+    @IBOutlet var nameField: UITextField!
+  
+    
 
-class ViewController: UIViewController {
+    @IBAction func displaylyrics(_ sender: Any) {
+        let maintemplate = ["<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+                            "Banana Fana Fo F<SHORT_NAME>",
+                            "Me My Mo M<SHORT_NAME>",
+                            "<FULL_NAME>"].joined(separator: "\n")
+        lyricsView.text = lyricforname(fullname: nameField.text!, template: maintemplate)
+        
+        
 
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        
+        textField.resignFirstResponder()
+        return false
+        
+        
+    }
+
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        
+        
+        view.addGestureRecognizer(tap)
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+        
+    }
+    
+    
+    
+    
+    @IBAction func reset(_ sender: Any) {
+        nameField.text = ""
+        lyricsView.text = ""
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
     }
-
-
-}
 
